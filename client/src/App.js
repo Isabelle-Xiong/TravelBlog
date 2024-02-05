1e5b711a57ef4ed8a873fcd040bc819b78555514
isabelle-xiong <yeelamisabelle.xiong@duke.edu>
2024-02-05 04:07:29 -0500

    added login and register page, todo: authentication

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout"
 import IndexPage from './pages/IndexPage';
 import Post from './Post';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Route path="/" element={<Layout />}>
         {/* Routes to Layout, which then Routes to either login page or posting page */}
         <Route index element={<IndexPage />} />
-        <Route path={'login'} element={<div>login page</div>} />
+        {/* when the URL matches /login, the LoginPage component should be rendered. */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,11 +1,11 @@
-import Post from './Post';
+import Post from "../Post";
 
 export default function IndexPage() {
     return (
-        <>
+        <div>
             <Post />
             <Post />
             <Post />
-        </>
-    )
+        </div>
+    );
 }
